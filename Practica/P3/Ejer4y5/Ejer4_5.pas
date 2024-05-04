cd621e66a59c38e6edd83e9408150da8fed5cf68
Damkerstar <damyoutu@gmail.com>
2024-05-04 07:33:01 -0300

    Added

--- Practica/P3/Ejer4y5/Ejer4_5.pas
+++ Practica/P3/Ejer4y5/Ejer4_5.pas
@@ -22,31 +22,74 @@ begin
 	end;
 end;
 
-procedure crearArch(var a: tArchFlores);
+procedure crearArch(var arch: tArchFlores);
 var
 	reg: reg_flor;
 begin
-	assign(a, 'flores');
-	rewrite(a);
+	assign(arch, 'flores');
+	rewrite(arch);
 	reg.codigo:= 0;
 	reg.nombre:= '';
-	while(codigo <> -1) do begin
-		write(a, reg);
+	while(reg.codigo <> -1) do begin
+		write(arch, reg);
 		leerFlor(reg);
 	end;
 	close(arch);
 end;
 
 
-// ELIMINAR ALGUN REGISTRO DEL ARCHIVO
+// ALTAS DE REGISTROS DEL ARCHIVO
+procedure agregarFlor(var arch: tArchFlores; nombre: string; codigo: integer);
+var
+	cabe, reg: reg_flor;
+begin
+	reset(arch);
+	read(arch, cabe);		// Leo el registro la cabecera
+	if(cabe.codigo < 0) then begin		// Si es menor a 0 significa que hay espacio
+		seek(arch, abs(cabe.codigo));	// Voy al campo del registro donde se ubicca el registro borrado
+		read(arch, cabe);				// Leo la nueva cabecera que puede tener 0 o otro registro borrado
+		seek(arch, filepos(arch)-1);	// Voy a la posicion del anterior registro
+		reg.codigo:= codigo;			// Agarro en un registro el codigo
+		reg.nombre:= nombre;				// y el nombre
+		write(arch, reg);				// Lo sobreescribo con el nuevo registro
+		seek(arch, 0);					// Vuelvo a la cabecera
+		write(arch, cabe);				// Sobreescribo la cabecera
+	end
+	else
+		writeln('No hay espacio en el archivo');
+	close(arch);
+end;
+
+
+// EJER B
+// LISTAR EL CONTENIDO DEL ARCHIVO
 procedure leerArch(var arch: tArchFlores; var reg: reg_flor);
 begin
 	if(not eof(arch)) then
 		read(arch, reg)
 	else
 		reg.codigo:= valorAlto;
-end
+end;
 
+procedure listarArch(var arch: tArchFlores);
+var
+	reg: reg_flor;
+begin
+	reset(arch);
+	leerArch(arch, reg);
+	while(reg.codigo <> valorAlto) do begin
+		if(reg.codigo > 0) then begin
+			writeln('Codigo: ', reg.codigo);
+			writeln('Nombre: ', reg.nombre);
+		end;
+		leerArch(arch, reg);
+	end;
+end;
+
+
+
+// EJERCICIO 5
+// ELIMINAR ALGUN REGISTRO DEL ARCHIVO
 procedure eliminarReg(var arch: tArchFlores);
 var
 	reg, cabe: reg_flor;
@@ -66,22 +109,16 @@ begin
 		seek(arch, 0);					// Me voy a la cabecera
 		reg.codigo:= reg.codigo*-1;		// Escribo el codigo al que tengo que eliminar con menos
 		write(arch, reg);				// Sobreescribo lo que esta en la cabecera por el registro eliminado
-	end;
+	end
+	else
+		writeln('No existe el codigo a borrar');
 	close(arch);
 end;
 
 
-// ALTAS DE REGISTROS DEL ARCHIVO
-procedure altaReg(var arch: tArchFlores);
-var
-	
-begin
-	
-end;
-
 
 var
-	arch: archivo;
+	arch: tArchFlores;
 	reg: reg_flor;
 BEGIN
 	writeln('CREAR ARCHIVO: ');
@@ -94,6 +131,9 @@ BEGIN
 	
 	writeln('AGREGAR REG AL ARCHIVO');
 	leerFlor(reg);
-	agregarFlor(arch, reg.codigo, reg.nombre);
+	agregarFlor(arch, reg.nombre, reg.codigo);
+	writeln;
+	
+	writeln('Lista del archivo sin archivos eliminados');
+	listarArch(arch);
 END.
-
